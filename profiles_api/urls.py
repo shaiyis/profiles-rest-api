b3c00b38fec1ca367011080314b3a8c195d9a83f
Israel Shai <shaiyis@gmail.com>
2024-11-10 13:13:12 +0200

    Added profiles viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,7 +4,13 @@ from profiles_api import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+
+"""
+need to specify base_name parameter for viewset that doesn't have a queryset
+or if we want to override the name of the queryset
+"""
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view', views.HelloApiView.as_view()),
